95e2bffc5ba74cd1eb514ee78b035c99cfa2d281
qzw <1870271825@qq.com>
2023-12-20 10:43:34 +0800

    e懒汉式单例模式

--- src/com/interface1/IFly.java
+++ src/com/interface1/IFly.java
@@ -3,4 +3,12 @@ package com.interface1;
 public interface IFly {
     // 飞行能力
     void fly();
+
+    default void fly2() {
+        System.out.println("fly2");
+    }
+
+    static void fly3() {
+        System.out.println("fly3");
+    }
 }
