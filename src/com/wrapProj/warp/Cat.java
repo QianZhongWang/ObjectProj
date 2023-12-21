00deb53462e6394177ea8617b6f140c922b6b299
qzw <1870271825@qq.com>
2023-12-21 14:08:31 +0800

    包装类

---
+++ src/com/wrapProj/warp/Cat.java
@@ -0,0 +1,8 @@
+package com.wrapProj.warp;
+
+public class Cat {
+    String name;
+    int age;
+    double weight;
+    Integer month;
+}
---
+++ src/com/wrapProj/warp/CatTest.java
@@ -0,0 +1,13 @@
+package com.wrapProj.warp;
+
+public class CatTest {
+    public static void main(String[] args) {
+        Cat catOne = new Cat();
+        System.out.println("小猫的昵称："+catOne.name);
+        System.out.println("小猫的年龄："+catOne.age);
+        System.out.println("小猫的体重："+catOne.weight);
+        System.out.println("小猫的月份："+catOne.month);
+
+
+    }
+}
