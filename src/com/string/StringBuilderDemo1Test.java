91d9329974ed21a5e2692e6654578a14c16be388
qzw <1870271825@qq.com>
2023-12-28 14:37:21 +0800

    字符串

--- src/com/string/StringBuilderDemo1Test.java
+++ src/com/string/StringBuilderDemo1Test.java
@@ -3,6 +3,13 @@ package com.string;
 public class StringBuilderDemo1Test {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("欢迎来到");
+        System.out.println(str);
         str.append("imooc");
+        System.out.println(str);
+        str.delete(0, 4).insert(0, "你好!");
+        System.out.println(str);
+        str.replace(0, 3, "你好，");
+        System.out.println(str);
+
     }
 }
