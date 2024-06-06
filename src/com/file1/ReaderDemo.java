682e0ea3e7cfecdc7b4b68cc575c2ac504430f3d
qzw <1870271825@qq.com>
2024-06-06 14:27:21 +0800

    使用缓存写文件和直接写文件的区别

--- src/com/file1/ReaderDemo.java
+++ src/com/file1/ReaderDemo.java
@@ -9,8 +9,10 @@ public class ReaderDemo {
         try {
             FileInputStream fis = new FileInputStream("imooc.txt");
             InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
             FileOutputStream fos = new FileOutputStream("imooc1.txt");
             OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
 
             int n = 0;
             char[] cbuf = new char[10];
@@ -22,9 +24,14 @@ public class ReaderDemo {
 //                System.out.println(new String(cbuf, 0, n));
 //            }
 
-            while ((n = isr.read(cbuf)) != -1) {
-                osw.write(cbuf, 0, n);
-                osw.flush();
+//            while ((n = isr.read(cbuf)) != -1) {
+//                osw.write(cbuf, 0, n);
+//                osw.flush();
+//            }
+
+            while ((n = br.read(cbuf)) != -1) {
+                bw.write(cbuf, 0, n);
+                bw.flush();
             }
 
 
@@ -32,6 +39,8 @@ public class ReaderDemo {
             isr.close();
             fos.close();
             osw.close();
+            br.close();
+            bw.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
